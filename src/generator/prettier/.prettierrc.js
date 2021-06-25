25022a5b247a120798324a1415697a5dd34aa03a
erguotou <erguotou525@gmail.com>
2021-06-25 16:43:15 +0800

    feat: ✨ 更新prettier配置文件

--- src/generator/prettier/.prettierrc.js
+++ src/generator/prettier/.prettierrc.js
@@ -1,43 +1,44 @@
+// https://prettier.io/docs/en/options.html
 module.exports = {
-  // https://prettier.io/docs/en/options.html
-  parser: 'babel',
-  // 一行最多 80 字符
-  // printWidth: 80,
+  // 一行最多 100 字符
+  printWidth: 100,
   // 使用 2 个空格缩进
-  // tabWidth: 2,
+  tabWidth: 2,
   // 不使用缩进符，而使用空格
-  // useTabs: false,
-  // 行尾不要有分号
+  useTabs: false,
+  // 行尾需要有分号
   semi: false,
   // 使用单引号
   singleQuote: true,
   // 对象的 key 仅在必要时用引号
-  // quoteProps: 'as-needed',
+  quoteProps: 'as-needed',
   // jsx 不使用单引号，而使用双引号
-  // jsxSingleQuote: false,
+  jsxSingleQuote: false,
   // 末尾不需要逗号
   trailingComma: 'none',
   // 大括号内的首尾需要空格
-  // bracketSpacing: true,
+  bracketSpacing: true,
   // jsx 标签的反尖括号需要换行
-  // jsxBracketSameLine: false,
-  // 箭头函数，避免使用括号
+  jsxBracketSameLine: false,
+  // 箭头函数，只有一个参数的时候，也需要括号
   arrowParens: 'avoid',
   // 每个文件格式化的范围是文件的全部内容
   rangeStart: 0,
   rangeEnd: Infinity,
+  // 解析器
+  parser: 'babel',
   // 不需要写文件开头的 @prettier
-  // requirePragma: false,
+  requirePragma: false,
   // 不需要自动在文件开头插入 @prettier
-  // insertPragma: false,
+  insertPragma: false,
   // 使用默认的折行标准
-  // proseWrap: 'preserve',
+  proseWrap: 'preserve',
   // 根据显示样式决定 html 要不要折行
-  // htmlWhitespaceSensitivity: 'css',
+  htmlWhitespaceSensitivity: 'css',
+  // Vue文件script和style标签内不缩进
+  vueIndentScriptAndStyle: false,
   // 换行符使用 auto
   endOfLine: 'auto',
-  // 格式化嵌入的内容
-  // embeddedLanguageFormatting: 'auto',
   overrides: [
     {
       files:
